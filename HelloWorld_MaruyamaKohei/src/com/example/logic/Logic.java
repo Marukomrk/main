c46d262d38f96629e0a1e31ebd22e5d166bf439b
Marukomrk <super_pokemon_777@ymail.ne.jp>
2023-06-11 01:39:46 +0900

    1_22 パッケージをコンパイル nullチェックを追記しまして再度提出

--- HelloWorld_MaruyamaKohei/src/com/example/logic/Logic.java
+++ HelloWorld_MaruyamaKohei/src/com/example/logic/Logic.java
@@ -14,8 +14,13 @@ public class Logic {
 		System.out.println("こんにちは！ここは日本です！");
 		System.out.println("この寿司はうまい");
 		System.out.println("寿司は和食です");
-		System.out.println("今の現在日時は" + formatDateTime(currentDateTime) + "です");
-	}
+		// nullチェック
+		if (currentDateTime != null) {
+            System.out.println("今の現在日時は" + formatDateTime(currentDateTime) + "です");
+        } else {
+            System.out.println("現在の日時を取得できませんでした");
+        }
+    }
 
 	private String formatDateTime(LocalDateTime dateTime) {
 		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
