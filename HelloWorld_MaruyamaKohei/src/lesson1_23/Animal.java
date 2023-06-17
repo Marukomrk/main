b6b98dc0ac8ee0079422af6a78884f6ae5d2963b
Marukomrk <super_pokemon_777@ymail.ne.jp>
2023-06-17 21:43:19 +0900

    課題1_23 修正提出

--- HelloWorld_MaruyamaKohei/src/lesson1_23/Animal.java
+++ HelloWorld_MaruyamaKohei/src/lesson1_23/Animal.java
@@ -1,30 +1,39 @@
 package lesson1_23;
 
 public class Animal {
-	private String name; // 動物の名前
-	private double length; // 体長（単位: m）
-	private double speed; // 速度（単位: km/h）
+	// 動物の名前
+	private String name;
+	// 体長（単位: m）
+	private double length;
+	// 速度（単位: km/h）
+	private double speed;
 
+	// 名前を取得するメソッド
 	public String getName() {
 		return name;
 	}
 
+	// 名前を設定するメソッド
 	public void setName(String name) {
 		this.name = name;
 	}
 
+	// 長さを取得するメソッド
 	public double getLength() {
 		return length;
 	}
 
+	// 長さを設定するメソッド
 	public void setLength(double length) {
 		this.length = length;
 	}
 
+	// 速度を取得するメソッド
 	public double getSpeed() {
 		return speed;
 	}
 
+	// 速度を設定するメソッド
 	public void setSpeed(double speed) {
 		this.speed = speed;
 	}
--- HelloWorld_MaruyamaKohei/src/lesson1_23/Main.java
+++ HelloWorld_MaruyamaKohei/src/lesson1_23/Main.java
@@ -10,16 +10,23 @@ package lesson1_23;
 
 public class Main {
 	public static void outputDetails(Animal animal) {
-		System.out.println("動物名：" + animal.getName()); // 動物名を出力
-		System.out.println("体長：" + animal.getLength() + "m"); // 体長を出力
-		System.out.println("速度：" + animal.getSpeed() + "km/h"); // 速度を出力
+		// 動物名を出力
+		System.out.println("動物名：" + animal.getName());
+		// 体長を出力
+		System.out.println("体長：" + animal.getLength() + "m");
+		// 速度を出力
+		System.out.println("速度：" + animal.getSpeed() + "km/h");
 	}
 
 	public static void main(String[] args) {
 		Animal animal = new Animal();
-		animal.setName("ライオン"); // 動物の名前を設定
-		animal.setLength(2.1); // 体長を設定
-		animal.setSpeed(80.0); // 速度を設定
-		outputDetails(animal); // 動物の詳細を出力
+		// 動物の名前を設定
+		animal.setName("ライオン");
+		// 体長を設定
+		animal.setLength(2.1);
+		// 速度を設定
+		animal.setSpeed(80.0);
+		// 動物の詳細を出力
+		outputDetails(animal);
 	}
 }
