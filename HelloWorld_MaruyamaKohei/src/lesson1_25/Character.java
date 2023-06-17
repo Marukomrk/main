f35e4676f2491815400c8b68490c2bb341366cc7
Marukomrk <super_pokemon_777@ymail.ne.jp>
2023-06-17 22:05:04 +0900

    課題1_25 修正提出

--- HelloWorld_MaruyamaKohei/src/lesson1_25/Character.java
+++ HelloWorld_MaruyamaKohei/src/lesson1_25/Character.java
@@ -1,18 +1,37 @@
 package lesson1_25;
 
 public class Character {
-	private int hp; // HP
-	private int mp; // MP
-	private int attack; // 攻撃力
-	private int agility; // 素早さ
-	private int defense; // 防御力
+	// 名前
+	private String name;
+	// HP
+	private int hp;
+	// MP
+	private int mp;
+	// 攻撃力
+	private int attack;
+	// 素早さ
+	private int agility;
+	// 防御力
+	private int defense;
 
-	public Character(int hp, int mp, int attack, int agility, int defense) {
+	// それぞれを初期化
+	public Character(String name, int hp, int mp, int attack, int agility, int defense) {
+		this.name = name;
 		this.hp = hp;
 		this.mp = mp;
 		this.attack = attack;
 		this.agility = agility;
 		this.defense = defense;
+	}
+
+	// 名前の取得
+	public String getName() {
+		return name;
+	}
+
+	// 名前の設定
+	public void setName(String name) {
+		this.name = name;
 	}
 
 	// HPの取得
@@ -64,4 +83,5 @@ public class Character {
 	public void setDefense(int defense) {
 		this.defense = defense;
 	}
+
 }
--- HelloWorld_MaruyamaKohei/src/lesson1_25/Player.java
+++ HelloWorld_MaruyamaKohei/src/lesson1_25/Player.java
@@ -3,39 +3,40 @@ package lesson1_25;
 import java.util.Random;
 
 public class Player extends Character {
-	private String name; // 名前
 
 	public Player(String name) {
-		super(0, 0, 0, 0, 0); // スーパークラスのコンストラクタを呼び出す
-		this.name = name; // 名前を設定する
+		// スーパークラスのコンストラクタを呼び出す
+		super(name, 0, 0, 0, 0, 0);
 	}
 
 	public void randomizeStats() {
 		Random random = new Random();
-		setHp(random.nextInt(1000) + 1); // HPをランダムに設定
-		setMp(random.nextInt(1000) + 1); // MPをランダムに設定
-		setAttack(random.nextInt(500) + 1); // 攻撃力をランダムに設定
-		setAgility(random.nextInt(1000) + 1); // 素早さをランダムに設定
-		setDefense(random.nextInt(50) + 1); // 防御力をランダムに設定
+		// HPをランダムに設定
+		setHp(random.nextInt(1000) + 1);
+		// MPをランダムに設定
+		setMp(random.nextInt(1000) + 1);
+		// 攻撃力をランダムに設定
+		setAttack(random.nextInt(500) + 1);
+		// 素早さをランダムに設定
+		setAgility(random.nextInt(1000) + 1);
+		// 防御力をランダムに設定
+		setDefense(random.nextInt(50) + 1);
 	}
 
 	public void outputDetails() {
-		System.out.println("こんにちは " + this.getName() + " さん"); // メッセージを出力
-		System.out.println("ステータス"); // メッセージを出力
-		System.out.println("HP：" + this.getHp()); // HPを出力
-		System.out.println("MP：" + this.getMp()); // MPを出力
-		System.out.println("攻撃力：" + this.getAttack()); // 攻撃力を出力
-		System.out.println("素早さ：" + this.getAgility()); // 素早さを出力
-		System.out.println("防御力：" + this.getDefense()); // 防御力を出力
-	}
-
-	// 名前を取得
-	public String getName() {
-		return name;
-	}
-
-	// 名前を設定
-	public void setName(String name) {
-		this.name = name;
+		// メッセージを出力
+		System.out.println("こんにちは " + this.getName() + " さん");
+		// メッセージを出力
+		System.out.println("ステータス");
+		// HPを出力
+		System.out.println("HP：" + this.getHp());
+		// MPを出力
+		System.out.println("MP：" + this.getMp());
+		// 攻撃力を出力
+		System.out.println("攻撃力：" + this.getAttack());
+		// 素早さを出力
+		System.out.println("素早さ：" + this.getAgility());
+		// 防御力を出力
+		System.out.println("防御力：" + this.getDefense());
 	}
 }
