f9e3016fa7444e2abd824761c6026415a629a2e9
Marukomrk <super_pokemon_777@ymail.ne.jp>
2023-06-17 21:27:34 +0900

    課題1_19 修正提出

--- HelloWorld_MaruyamaKohei/src/basicClass/Animal.java
+++ HelloWorld_MaruyamaKohei/src/basicClass/Animal.java
@@ -7,15 +7,25 @@ public class Animal {
 
 	public static void main(String[] args) {
 		// Q1：Dogクラスを呼び出して変数に代入し、動物の名前をコンソールに出力してください。
+		// Dogクラスをインスタンス化し、dog1変数に代入
 		Dog dog1 = new Dog();
+		// dog1の名前を出力
 		System.out.println("動物の名前: " + dog1.getName());
+
 		// Q2：Dogクラスを呼び出して変数に代入し、動物の数をコンソールに出力してください。
+		// 引数を指定してDogクラスをインスタンス化し、dog2変数に代入
 		Dog dog2 = new Dog(5);
+		// dog2の数を出力
 		System.out.println("動物の数: " + dog2.getCount());
+
 		// 現在の日時を「yyyy-MM-dd H:m:s」形式で変数に代入して、コンソールに出力してください
+		// 現在の日時を取得
 		LocalDateTime now = LocalDateTime.now();
+		// 日時の表示形式を指定
 		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd H:m:s");
+		// 日時を指定した形式でフォーマット
 		String formattedDateTime = now.format(formatter);
+		// フォーマットされた日時を出力
 		System.out.println("現在の日時: " + formattedDateTime);
 	}
 }
--- HelloWorld_MaruyamaKohei/src/basicClass/Dog.java
+++ HelloWorld_MaruyamaKohei/src/basicClass/Dog.java
@@ -2,25 +2,31 @@ package basicClass;
 
 public class Dog {
 	// Q1：フィールドに動物の名前の変数を定義してください。
+	// 動物の名前を表す変数
 	private String name;
 
 	// Q2：フィールドに動物の数の変数を定義してください。
+	// 動物の数を表す変数
 	private int count;
 
 	// Q3：Q1で作成した変数に「犬」を代入するコンストラクタを作成してください。
+	// コンストラクタで変数に"犬"を代入
 	public Dog() {
 		name = "犬";
 	}
 
 	// Q4：Q2で作成した変数に引数を代入するコンストラクタを作成してください。
+	// 引数を受け取って変数に代入
 	public Dog(int count) {
 		this.count = count;
 	}
 
+	// 変数の値を返す
 	public String getName() {
 		return name;
 	}
 
+	// 変数の値を返す
 	public int getCount() {
 		return count;
 	}
