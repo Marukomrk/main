4bd410c3401517fb690c0d63c1848a40731c05a5
Marukomrk <super_pokemon_777@ymail.ne.jp>
2023-06-26 22:58:39 +0900

    課題1_29 修正提出

--- HelloWorld_MaruyamaKohei/src/lesson1_29/practice/main/Main.java
+++ HelloWorld_MaruyamaKohei/src/lesson1_29/practice/main/Main.java
@@ -35,31 +35,79 @@
 
 package lesson1_29.practice.main;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Scanner;
 
 import lesson1_29.practice.sort.Sorter;
 
 public class Main {
 	public static void main(String[] args) {
+		// 都道府県情報のリストを作成
+		List<Sorter> prefectures = new ArrayList<>();
+		// 都道府県情報をリストに追加
+		prefectures.add(new Sorter("北海道", "札幌市", 83424));
+		prefectures.add(new Sorter("青森県", "青森市", 9646));
+		prefectures.add(new Sorter("岩手県", "盛岡市", 15275));
+		prefectures.add(new Sorter("宮城県", "仙台市", 7282));
+		prefectures.add(new Sorter("秋田県", "秋田市", 11638));
+		prefectures.add(new Sorter("山形県", "山形市", 9323));
+		prefectures.add(new Sorter("福島県", "福島市", 13784));
+		prefectures.add(new Sorter("茨城県", "水戸市", 6097));
+		prefectures.add(new Sorter("栃木県", "宇都宮市", 6408));
+		prefectures.add(new Sorter("群馬県", "前橋市", 6362));
+		prefectures.add(new Sorter("埼玉県", "さいたま市", 3798));
+
+		// ユーザーからの入力を受け取るためのScannerオブジェクトを作成
 		Scanner scanner = new Scanner(System.in);
+		System.out.print("都道府県の配列インデックスを入力してください（カンマ区切り）: ");
+		// ユーザーからの入力を文字列として受け取る
+		String indexInput = scanner.nextLine();
+		System.out.print("並び替え方を選択してください（1: 昇順、2: 降順）: ");
+		// ユーザーからの入力を文字列として受け取る
+		String orderInput = scanner.nextLine();
 
-		// 都道府県の配列インデックスを入力
-		System.out.println("都道府県の配列インデックスを入力してください（カンマ区切り）:");
-		String inputIndices = scanner.nextLine();
+		if (!indexInput.isEmpty() && !orderInput.isEmpty()) {
+			// インデックスの入力値をカンマで分割し、配列として格納
+			String[] indices = indexInput.split(",");
+			// 選択されたインデックスのリストを作成
+			List<Integer> selectedIndices = new ArrayList<>();
 
-		// 並び替え方を選択
-		System.out.println("並び替え方を選択してください（1: 昇順、2: 降順）:");
-		int sortOrder = scanner.nextInt();
+			for (String index : indices) {
+				try {
+					// 文字列を整数に変換
+					int num = Integer.parseInt(index);
+					if (num >= 0 && num < prefectures.size()) {
+						// 入力された有効なインデックスを選択リストに追加
+						selectedIndices.add(num);
+					}
+					// インデックスが数値ではない場合は無視
+				} catch (NumberFormatException e) {
+				}
+			}
 
-		// Sorterオブジェクトの生成
-		Sorter sorter = new Sorter(inputIndices);
+			if (orderInput.equals("1")) {
+				// 昇順で選択リストをソート
+				Collections.sort(selectedIndices);
+			} else if (orderInput.equals("2")) {
+				// 降順で選択リストをソート
+				Collections.sort(selectedIndices, Collections.reverseOrder());
+			}
 
-		// 都道府県情報を並び替えて取得
-		String[] sortedPrefectures = sorter.sortPrefectures(sortOrder == 1);
-
-		// 並び替え結果の出力
-		sorter.printPrefectures(sortedPrefectures);
-
+			// 選択されたインデックスに対応する都道府県情報を出力
+			for (int selectedIndex : selectedIndices) {
+				Sorter prefecture = prefectures.get(selectedIndex);
+				System.out.println("都道府県名：" + prefecture.getName());
+				System.out.println("県庁所在地：" + prefecture.getCapital());
+				System.out.println("面積：" + prefecture.getArea() + "km2");
+				System.out.println();
+			}
+		} else {
+			System.out.println("入力が無効です。");
+		}
+		
+		// Scannerをクローズ
 		scanner.close();
 	}
 }
--- HelloWorld_MaruyamaKohei/src/lesson1_29/practice/sort/Sorter.java
+++ HelloWorld_MaruyamaKohei/src/lesson1_29/practice/sort/Sorter.java
@@ -1,74 +1,35 @@
 package lesson1_29.practice.sort;
 
-import java.util.Arrays;
-import java.util.Comparator;
-
 public class Sorter {
-	// 都道府県の情報を配列で保持
-	private static final String[] prefectures = {
-			"北海道:札幌市:83424",
-			"青森県:青森市:9646",
-			"岩手県:盛岡市:15275",
-			"宮城県:仙台市:7282",
-			"秋田県:秋田市:11638",
-			"山形県:山形市:9323",
-			"福島県:福島市:13784",
-			"茨城県:水戸市:6097",
-			"栃木県:宇都宮市:6408",
-			"群馬県:前橋市:6362",
-			"埼玉県:さいたま市:3798"
-	};
+	// 都道府県名を表す変数
+	private String name;
+	// 県庁所在地を表す変数
+	private String capital;
+	// 面積を表す変数
+	private double area;
 
-	// 選択されたインデックスを格納する配列
-	private int[] indices;
-
-	public Sorter(String inputIndices) {
-		// 入力されたインデックスをカンマで分割して配列に格納
-		String[] indicesString = inputIndices.split(",");
-		indices = new int[indicesString.length];
-
-		// 文字列を整数に変換してインデックス配列に格納
-		for (int i = 0; i < indicesString.length; i++) {
-			indices[i] = Integer.parseInt(indicesString[i].trim());
-		}
+	// コンストラクタの引数を使って、インスタンスの各変数を初期化する
+	public Sorter(String name, String capital, double area) {
+		// 都道府県名を設定
+		this.name = name;
+		// 県庁所在地を設定
+		this.capital = capital;
+		// 面積を設定
+		this.area = area;
 	}
 
-	public String[] sortPrefectures(boolean ascending) {
-		// ソートされた都道府県の情報を格納するための配列を作成
-		String[] sortedPrefectures = new String[indices.length];
-
-		// 指定されたインデックスに対応する都道府県情報をソート配列にコピー
-		for (int i = 0; i < indices.length; i++) {
-			sortedPrefectures[i] = prefectures[indices[i]];
-		}
-
-		// 都道府県名を基準に昇順でソート
-		Arrays.sort(sortedPrefectures, Comparator.comparing(p -> p.split(":")[0]));
-
-		// 降順を指定された場合は配列を逆順にソート
-		if (!ascending) {
-			Arrays.sort(sortedPrefectures, Comparator.reverseOrder());
-		}
-
-		// ソートされた都道府県の情報を返す
-		return sortedPrefectures;
+	public String getName() {
+		// 都道府県名を取得して返す
+		return name;
 	}
 
-	public void printPrefectures(String[] prefectures) {
+	public String getCapital() {
+		// 県庁所在地を取得して返す
+		return capital;
+	}
 
-		for (String prefecture : prefectures) {
-			// 都道府県情報を分割
-			String[] parts = prefecture.split(":");
-			String prefectureName = parts[0];
-			String city = parts[1];
-			double area = Double.parseDouble(parts[2]) / 1;
-
-			// 都道府県名を表示
-			System.out.println("都道府県名：" + prefectureName);
-			// 県庁所在地を表示
-			System.out.println("県庁所在地：" + city);
-			// 面積を表示（小数点以下1桁の形式で表示）
-			System.out.println("面積：" + area + "km2\n");
-		}
+	public double getArea() {
+		// 面積を取得して返す
+		return area;
 	}
 }
